354c207481ea38cdae6bd2914b231b567fe4b37f
William D <hardworkday59@gmail.com>
2017-01-31 17:20:24 -0500

    Finished lab

--- src/RoomCalc.java
+++ src/RoomCalc.java
@@ -4,26 +4,27 @@ import java.util.Scanner;
  * Created by williamdonaldson on 1/30/17.
  */
 public class RoomCalc {
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println(" Enter length ");
-        double length = scan.nextDouble();
+        int choose = 1;
 
-        System.out.println(" Enter Width ");
+        while (choose == 1) {
+            System.out.println(" Enter length ");
+            double length = scan.nextDouble();
 
-        double width = scan.nextDouble();
+            System.out.println(" Enter Width ");
 
-        System.out.println("Enter Area");
+            double width = scan.nextDouble();
 
-        double area = scan.nextDouble();
+            double area = (length * width);
+            System.out.println("Area: " + length * width);
 
-        double perimeter = (length * width);
-        System.out.println ("the perimeter is " + (length * 2 + width * 2));
+            System.out.println("Perimeter: " + (length * 2 + width * 2));
 
-        System.out.println("Continue? (y/n)");
-
-        scan.close();
+            System.out.println("Continue? enter 1 for yes or 2 for no");
+            choose = scan.nextInt();
+        }
     }
 }
